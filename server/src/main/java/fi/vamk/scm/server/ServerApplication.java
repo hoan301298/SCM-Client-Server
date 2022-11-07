f089d8857c369fffd43dd55478c6f2ad45227cf0
Timo Kankaanpää <tka@puv.fi>
2022-11-07 10:46:09 +0200

    Plain server

---
+++ server/src/main/java/fi/vamk/scm/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package fi.vamk.scm.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
