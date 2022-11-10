0d4c490558ba8699711c18b3284a70ae82a0ca15
Timo Kankaanpää <tka@puv.fi>
2022-11-10 10:54:09 +0200

    Added todos for next time

--- client/src/containers/AboutContainer.tsx
+++ client/src/containers/AboutContainer.tsx
@@ -1,6 +1,14 @@
 import { FC } from "react";
 import AboutComponent from "../components/AboutComponent";
 
+/**TODO
+ * 1. Move state management to container
+ * 2. Pass data from conainer to controller
+ * 3. Create type for keyvalue
+ * 4. Create test cases
+ * 5. Fix the test CI pipeline
+ * 6. Create CD pipeline
+ */
 const AboutContainer:FC=()=>{
     return(
         <div>
